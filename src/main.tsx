7919000f18463dff79f7d9333bec7849aceb1e85
phucngo2 <phucnnh21@gmail.com>
2024-07-13 10:56:09 +0700

    Chore(3): Configure vite-tsconfig-paths

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,9 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 // core styles are required for all packages
+import { theme } from "@client/config";
 import { MantineProvider } from "@mantine/core";
 import "@mantine/core/styles.css";
-import { theme } from "./config";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
